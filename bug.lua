4c269a34c39262dc845088a67a7a10f1d0893819
BugHunter-X <askzenai@gmail.com>
2025-02-22 08:50:05 +0100

    Add bug.lua file

---
+++ bug.lua
@@ -0,0 +1,10 @@
+local function foo(a, b)
+  if a == nil then return nil end
+  if b == nil then return nil end
+  return a + b
+end
+
+print(foo(10, 20)) -- Output: 30
+print(foo(nil, 20)) -- Output: nil
+print(foo(10, nil)) -- Output: nil
+print(foo(nil, nil)) -- Output: nil
